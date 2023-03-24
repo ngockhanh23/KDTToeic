859fcc896ec5cede10edc0de862591b0526b1877
NAD-2K3 <anhduc19112003@gmail.com>
2023-03-24 11:22:36 +0700

    update fragment_test.xml

---
+++ app/src/main/java/com/example/kdttoeic/adapter/TestAdapter.java
@@ -0,0 +1,71 @@
+package com.example.kdttoeic.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.kdttoeic.R;
+import com.example.kdttoeic.model.Test;
+
+import java.util.List;
+
+public class TestAdapter extends RecyclerView.Adapter<TestAdapter.TestViewHolder> {
+    private List<Test> myTest;
+   public Listener listener;
+    public TestAdapter(List<Test> myTest, Listener listener) {
+        this.listener = listener;
+        this.myTest = myTest;
+    }
+
+    @NonNull
+    @Override
+    public TestViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_test, parent, false);
+
+        return new TestViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TestViewHolder holder, int position) {
+        Test test = myTest.get(position);
+        if(test == null) {
+            return;
+        }
+
+        holder.testTitle.setText(test.getTitle());
+        holder.testDes.setText(test.getDescription());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.OnOpenExam(position, test);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return myTest.size();
+    }
+
+    public static class TestViewHolder extends RecyclerView.ViewHolder {
+        private TextView testTitle;
+        private TextView testDes;
+
+        public TestViewHolder(@NonNull View itemView) {
+            super(itemView);
+            testTitle = itemView.findViewById(R.id.testTitle);
+            testDes = itemView.findViewById(R.id.testDes);
+        }
+    }
+
+    public interface Listener{
+        void OnOpenExam(int pos, Test test);
+    }
+}
---
+++ app/src/main/java/com/example/kdttoeic/model/Test.java
@@ -0,0 +1,29 @@
+package com.example.kdttoeic.model;
+
+public class Test {
+    private String title;
+    private String description;
+    private String mucde;
+
+    public Test(String title, String description, String mucde) {
+        this.title = title;
+        this.description = description;
+        this.mucde = mucde;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
